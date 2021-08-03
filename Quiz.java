c2f81772262a060f6d655c64eb86ac97978928a0
Kokodaks <dayeonko357@gmail.com>
2021-08-03 22:52:01 +0900

    Create Quiz.java
    final

---
+++ Quiz.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class Quiz {
+
+    // public String printTriangle (int num){
+    //     for(int i =0; i<num; i++) {
+    //         for(int j=0; j<num; j++){
+    //             System.err.print();
+    //         }
+    //         System.out.println();
+    //     }
+    //     return "";
+    // }
+
+
+    // public static void main(String[] args) {
+    //     PrintTriangle pt = new PrintTriangle();
+    //     System.out.println(pt.printTriangle(3));
+    // }
+//     public static void main(String[] args) {
+//         int row = 4;
+//         for (int i = 0; i < row ; i++) {
+//             System.out.println("*");
+//             for (int j = 0; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//         }
+//     }
+
+
+    public String printTriangle (int num){
+        for(int i =0; i<num; i++) {
+            for(int j=0; j<num; j++){
+                System.err.println();
+            }
+            System.out.println();
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int rightTriangle;
+        rightTriangle = stdIn.nextInt();
+
+        for (int a = 0; a < rightTriangle; a++) {
+            
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
